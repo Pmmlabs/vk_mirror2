eb3ac3c34a1ca46cae80712614df9a7235eae212
Pmmlabs <kilowatt36@mail.ru>
2015-11-04 14:16:34 +0300

    xhr download[2]

--- cron.sh
+++ cron.sh
@@ -3,5 +3,6 @@
 git pull
 
 wget -x -nv -w 1 --random-wait -i urls.txt
+./xhr.sh
 
 git commit -a -m "$(date +"%x %T")" && git push
--- xhr.sh
+++ xhr.sh
@@ -1,9 +1,11 @@
+#!/bin/bash
 
 array=(
 "vk.com/al_video.php?act=show&al=1&autoplay=1&module=video&show_next=1&video=-22822305_171359037"
 )
 for url in ${array[@]}
 do
-    wget -O - "http://$url" | awk -F '<!>' 'BEGIN{RS="~"}{print $8}' > $url
+    wget -nv -O - "http://$url" | awk -F '<!>' 'BEGIN{RS="~"}{print $8}' | iconv -f CP1251 -t UTF8 > $url
+    sleep 1
 done
 
