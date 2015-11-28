6931136c188e3e138ebe0a6facbd0ac27cd8d0ee
Pmmlabs <kilowatt36@mail.ru>
2015-11-28 18:54:24 +0300

    nelive

--- xhr.sh
+++ xhr.sh
@@ -3,6 +3,7 @@
 array=(
 "vk.com/al_video.php?act=show&al=1&autoplay=1&module=video&show_next=1&video=-22822305_171359037"
 "vk.com/al_video.php?act=show&al=1&autoplay=1&module=video&show_next=1&video=53083705_161242013"
+"vk.com/al_profile.php?__query=nelive&al=1"
 )
 for url in ${array[@]}
 do
