c97b726380e932881465b858ee50e8e20700828d
Investocenka <kilowatt36@yandex.ru>
2016-04-01 16:08:58 +0300

    01-04-2016 16:08:56

--- vk.com/js/al/doge.js
+++ vk.com/js/al/doge.js
@@ -112,4 +112,113 @@ blowImages: function() {
   });
 },
 
-eof:1};try{stManager.done('doge.js');}catch(e){}
+eof:1};
+
+var Harold = {
+
+width: 180, // 360
+height: 128, // 256
+offset: -30,
+
+ttwidth: 242,
+ttheight: 105,
+ttoffset: -73,
+
+init: function(lang, hash) {
+  this.hash = hash;
+
+  var loaded = 0,
+    onload = function() {
+      if (++loaded != 2) return;
+      if (window.IdleManager) {
+        var idleManager = new IdleManager({
+          onIdleCb: function () {
+            this.appear();
+            idleManager.stop();
+          }.bind(this),
+          id: 'harold',
+          idleTimeout: 1000,
+          element: document,
+          focusElement: window
+        });
+        idleManager.start();
+        idleManager.activate();
+      } else {
+        this.appear();
+      }
+    }.bind(this);
+
+  this.obj = ce('a', {href: 'https://new.vk.com/blog', target: '_blank'}, {cursor: 'pointer', position: 'fixed', bottom: -this.height, right: 70, width: this.width, height: this.height, zIndex: 110});
+  this.tt = ce('img', {width: this.ttwidth, height: this.ttheight, 'className':'harold-tt'}, {top: -this.offset * 2, left: -210, 'pointer-events': 'none', position: 'absolute'});
+  var img = ce('img', {width: this.width, height: this.height, 'className':'harold-main'});
+
+  img.onload = onload;
+  img.src = '/images/pics/Harold'+(window.devicePixelRatio >= 2 ? '_2x' : '')+'.png';
+  this.tt.onload = onload;
+  this.tt.src = '/images/pics/Harold_str_'+lang+(window.devicePixelRatio >= 2 ? '_2x' : '')+'.png';
+
+  this.obj.appendChild(this.tt);
+  this.obj.appendChild(img);
+  utilsNode.appendChild(this.obj);
+},
+
+appear: function() {
+  animate(this.obj, {bottom: -(this.height + this.offset)}, {
+    duration: 1500,
+    transition:function(t, b, c, d) { return c * cubicBezier(.47,4.29,.51,.3, t/d, 4/d) + b; }
+  });
+  var onshow = this.show.bind(this),
+    onhide = this.hide.bind(this),
+    onclick = this.click.bind(this, onshow, onhide);
+  addEvent(this.obj, 'mouseover', onshow);
+  addEvent(this.obj, 'mouseout', onhide);
+  addEvent(this.obj, 'click', onclick);
+  addEvent(this.tt, 'click', onclick);
+},
+
+show: function() {
+  this.ht  && clearTimeout(this.ht);
+  animate(this.obj, {bottom: 0}, {
+    duration: 400,
+    transition: Fx.Transitions.swiftOut
+  });
+  this.tttm = setTimeout(function(){
+      animate(this.tt, {top: this.ttoffset}, {
+      duration: 150,
+      transition: Fx.Transitions.swiftOut,
+      onComplete: function() {
+        this.tt.style.pointerEvents = 'all';
+      }.bind(this)
+    });
+  }.bind(this), 150);
+},
+
+hide: function() {
+  this.tttm  && clearTimeout(this.tttm);
+  this.ht  && clearTimeout(this.ht);
+  this.ht = setTimeout(function() {
+    this.tt.style.pointerEvents = 'none';
+    animate(this.tt, {top: -this.offset * 2}, {duration: 150, transition: function(t, b, c, d) { return c * cubicBezier(1,-0.01,.96,.77, t/d, 4/d) + b; }});
+    animate(this.obj, {bottom: -(this.height + this.offset)}, 200);
+  }.bind(this), 100);
+},
+
+click: function(onshow, onhide) {
+  if (this.clicked) return;
+  this.tttm  && clearTimeout(this.tttm);
+  this.clicked = true;
+  removeEvent(this.obj, 'mouseover', onshow);
+  removeEvent(this.obj, 'mouseout', onhide);
+  this.obj.style.pointerEvents = 'none';
+  animate(this.tt, {top: -this.offset * 2}, {duration: 150, transition: function(t, b, c, d) { return c * cubicBezier(1,-0.01,.96,.77, t/d, 4/d) + b; }});
+  animate(this.obj, {bottom: -this.height}, 200);
+  ajax.post('al_index.php', {
+    act: 'a_testredesign',
+    hash: this.hash,
+    harold: 1
+  });
+},
+
+eof:1};
+
+try{stManager.done('doge.js');}catch(e){}
