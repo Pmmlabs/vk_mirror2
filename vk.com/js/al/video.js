758fc3c17d9805330be9046d061a731d3638d214
git <git@.(none)>
2016-04-07 11:13:01 +0300

    07-04-2016 11:12:53

--- vk.com/js/al/video.js
+++ vk.com/js/al/video.js
@@ -1463,7 +1463,7 @@ var Video = {
       ext: cur.vExt,
       order: cur.vOrder
     }, {
-      onDone: (function(count, data, playlists) {
+      onDone: (function(count, data, playlists, realOffset) {
         removeClass(cur.videoSearch, 'v_loading');
         removeClass(cur.more, 'load_more');
         data = eval('('+data+')');
@@ -1518,7 +1518,11 @@ var Video = {
             Video.initSorter();
           }
         }
-        searchData.offset += data.length;
+        if (realOffset) {
+          searchData.offset = realOffset;
+        } else {
+          searchData.offset += data.length;
+        }
         this.changeSummary();
 
         re('video_featured_playlists');
