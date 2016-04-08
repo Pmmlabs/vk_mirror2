1a94231a1a9a321dd7a2beba2439533dd5ee22d6
git <git@.(none)>
2016-04-08 16:07:50 +0300

    08-04-2016 16:07:47

--- vk.com/js/al/video_youtube.js
+++ vk.com/js/al/video_youtube.js
@@ -380,14 +380,6 @@ var VideoYoutube = {
   },
 
   onError: function(event) {
-    var videoUnavaiableErrors = [100, 101, 150];
-    if (videoUnavaiableErrors.indexOf(event.data) != -1) {
-      ajax.post('/al_video.php', {
-        act: 'reparseDeletedYoutube',
-        vid: VideoYoutube.cur.vars.vid,
-        oid: VideoYoutube.cur.vars.oid
-      });
-    }
   },
 
 
