cf49aacd52f3984aa8e314c155730430bdd49568
git <git@.(none)>
2016-10-11 16:10:48 +0300

    11-10-2016 16:10:44

--- vk.com/js/al/common.js
+++ vk.com/js/al/common.js
@@ -8468,7 +8468,7 @@ function mentionOver(el, opts) {
     checkLeft: true,
     reverseOffset: opts.reverseOffset || 112,
     dir: 'auto',
-    appendEl: domClosest('_im_mess_stack', el) || domClosest('rb_box_wrap', el)
+    appendEl: domClosest('_im_peer_history', el) || domClosest('rb_box_wrap', el)
       || domClosest('wk_cont', el) || domClosest('scroll_fix_wrap', el)
   });
 }
@@ -9309,7 +9309,7 @@ function audioShowActionTooltip(btn) {
 
   if (gpeByClass('_im_mess_stack', btn)) {
     options.appendParentCls = '_im_mess_stack';
-    options.shift = [13, 10, 0];
+    options.shift = [7, 10, 0];
     options.noZIndex = true;
 
   } else if (gpeByClass('top_notify_wrap', btn)) {
