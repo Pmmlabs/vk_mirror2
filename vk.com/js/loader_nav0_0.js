de9bda320051b3d679b586f55e8702308844c154
Pmmlabs <vk_mirror@pmmlabs.ru>
2019-05-15 16:12:21 +0300

    15.05.2019 16:12:21

--- vk.com/js/loader_nav0_0.js
+++ vk.com/js/loader_nav0_0.js
@@ -711,7 +711,7 @@ var stTypes = {
         'apps_edit.js': 1
     }
 };
-var _rnd = 5105;
+var _rnd = 4265;
 var stDeps = {
     "/js/cmodules/web/GroupsEdit.js": ["cmodules/bundles/vendors.js"],
     "/js/cmodules/web/addresses.js": ["cmodules/bundles/common.js"],
